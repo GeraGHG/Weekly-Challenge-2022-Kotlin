049e5f03c535654acd2ad652a34987e0b274933b
GeraGHG <gera.GHGhernandez@gmail.com>
2023-06-18 13:00:19 -0600

    Solución Reto #3 Python

--- 0.3Numeroprimo.py
+++ 0.3Numeroprimo.py
@@ -1,3 +1,9 @@
+'''
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+'''
 numero = int(input("Ingrese un número para comprobar si es primo o no: "))
 if numero % 2 == 1: print(f"\t\"{numero}\" es primo")
 print(f"Los números primos del 1 al 100 son:\n {[x for x in range(1, 101) if x % 2 == 1]}")
